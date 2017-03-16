f325edcc884a94412ae60bd179b3ec7a2f8b371b
meyerbaptiste <baptiste@les-tilleuls.coop>
2017-03-16 16:41:46 +0100

    Use iconv_set_encoding() for PHP < 5.6

--- src/MarketplaceWebService/Client.php
+++ src/MarketplaceWebService/Client.php
@@ -97,7 +97,13 @@ class MarketplaceWebService_Client implements MarketplaceWebService_Interface
         $applicationVersion,
         $attributes = null
     ) {
-        ini_set('default_charset', 'UTF-8');
+        if (function_exists('iconv') && PHP_VERSION_ID < 50600) {
+            iconv_set_encoding('output_encoding', 'UTF-8');
+            iconv_set_encoding('input_encoding', 'UTF-8');
+            iconv_set_encoding('internal_encoding', 'UTF-8');
+        } else {
+            ini_set('default_charset', 'UTF-8');
+        }
 
         $this->awsAccessKeyId = $awsAccessKeyId;
         $this->awsSecretAccessKey = $awsSecretAccessKey;
